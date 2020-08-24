5455ad187bd99b08e1d3b347add2a0359bbe7edd
uhobeike <tatuhiro315@ezweb.ne.jp>
2020-08-24 20:23:04 +0900

    waypoint_rviz_set marker_remove module add finish

--- turtlebot3/turtlebot3_goal_send/src/waypoint_rviz_set.cpp
+++ turtlebot3/turtlebot3_goal_send/src/waypoint_rviz_set.cpp
@@ -1,5 +1,7 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
+#include <geometry_msgs/Point.h>
+#include <std_msgs/ColorRGBA.h>
 #include <geometry_msgs/PoseArray.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <nav_msgs/Odometry.h>
@@ -19,6 +21,7 @@ using std::to_string;
 using std::ofstream;
 using std::cout;
 using std::endl;
+using std::find;
 
 class waypoint_rviz
 {
@@ -27,29 +30,33 @@ class waypoint_rviz
     ros::Publisher way_pub_, marker_pub;
     ros::Subscriber subscriber_set_, subscriber_control_;
 
+    geometry_msgs::Point point_;
     geometry_msgs::PoseArray pose_array_;
     geometry_msgs::Pose pose_rviz_;
-
+    std_msgs::ColorRGBA point_color_;
     visualization_msgs::Marker marker_;
 
     vector<string> posi_set_;
     vector<vector<string>> csv_array_;
+    vector<uint16_t> waypoint_marker_id_;
 
     uint16_t waypoint_number_;
     uint16_t waypoint_index_;
     uint32_t shape_;
+    bool marker_mode_;
     bool marker_flag_;
 
     void waypoint_Callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& pose);
     void control_Callback(const std_msgs::StringConstPtr& command);
 
     void waypoint_csv_(vector<string>& posi_set, vector<vector<string>>& csv_array, uint16_t &waypoint_number);
-    void way_point_remove_(vector<vector<string>>& waypoint_remove, geometry_msgs::PoseArray& pose_array, uint16_t &waypoint_number);
+    void way_point_remove_(vector<vector<string>>& waypoint_remove, geometry_msgs::PoseArray& pose_array, visualization_msgs::Marker& marker, uint16_t &waypoint_number);
     void way_point_goal_set_(vector<vector<string>>& waypoint_goal, uint16_t &waypoint_number);
     void way_point_corner_set_(vector<vector<string>>& waypoint_corner, uint16_t &waypoint_number);
     void finish_and_file_write_waypoint_(vector<vector<string>>& waypoint_file_write, uint16_t &waypoint_number);
-    void waypoint_marker_(visualization_msgs::Marker& marker, vector<vector<string>>& waypoint_goal_corner, bool &marker_flag, uint16_t &waypoint_number);
-
+    void waypoint_marker_(visualization_msgs::Marker& marker, geometry_msgs::Point& point, std_msgs::ColorRGBA& point_color, vector<vector<string>>& waypoint_goal_corner, bool &marker_mode, uint16_t &waypoint_number);
+    bool waypoint_marker_id_check(vector<uint16_t>& waypoint_marker_id, uint16_t &waypoint_number);
+    
   public:
     waypoint_rviz();
     ~waypoint_rviz(){};
@@ -69,9 +76,10 @@ waypoint_rviz::waypoint_rviz() : nh_("")
   };
 
   waypoint_number_ = 0;
+  marker_mode_ = 0;
   marker_flag_ = 0;
 
-  shape_ = visualization_msgs::Marker::SPHERE;
+  shape_ = visualization_msgs::Marker::POINTS;
 }
 
 void waypoint_rviz::waypoint_Callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& pose)
@@ -101,7 +109,7 @@ void waypoint_rviz::control_Callback(const std_msgs::StringConstPtr& command)
 {
   if(command->data == "remove")
   {
-    way_point_remove_(csv_array_, pose_array_, waypoint_number_);
+    way_point_remove_(csv_array_, pose_array_, marker_,waypoint_number_);
   }
 
   else if(command->data == "goal")
@@ -124,12 +132,9 @@ void waypoint_rviz::waypoint_csv_(vector<string>& posi_set, vector<vector<string
 {
   csv_array.push_back(posi_set);
   waypoint_number++;
-
-  cout << csv_array.size() << endl;
-  cout << waypoint_number_ << endl;
 }
 
-void waypoint_rviz::way_point_remove_(vector<vector<string>>& waypoint_remove, geometry_msgs::PoseArray& pose_array, uint16_t &waypoint_number)
+void waypoint_rviz::way_point_remove_(vector<vector<string>>& waypoint_remove, geometry_msgs::PoseArray& pose_array, visualization_msgs::Marker& marker, uint16_t &waypoint_number)
 {
   ROS_INFO("way_point_remove");
 
@@ -137,12 +142,19 @@ void waypoint_rviz::way_point_remove_(vector<vector<string>>& waypoint_remove, g
   {
     waypoint_number--;
     waypoint_remove.resize(waypoint_number);
-    pose_array_.poses.resize(waypoint_number);
-  }
-  cout << csv_array_.size() << endl;
-  cout << waypoint_number << endl;
+    pose_array.poses.resize(waypoint_number);
 
-  way_pub_.publish(pose_array_);
+    way_pub_.publish(pose_array_);
+    cout << waypoint_remove.size() << endl; 
+  }
+
+  if(waypoint_marker_id_check(waypoint_marker_id_, waypoint_number) )
+  {
+    marker_.points.pop_back();
+    marker.colors.pop_back();
+
+    marker_pub.publish(marker);
+  }
 }
 
 void waypoint_rviz::way_point_goal_set_(vector<vector<string>>& waypoint_goal, uint16_t &waypoint_number)
@@ -150,8 +162,8 @@ void waypoint_rviz::way_point_goal_set_(vector<vector<string>>& waypoint_goal, u
   uint16_t array_number = waypoint_number - 1;
   waypoint_goal[array_number].push_back("goal");
 
-  marker_flag_ = 0;
-  waypoint_marker_(marker_, waypoint_goal, marker_flag_, waypoint_number);
+  marker_mode_ = 0;
+  waypoint_marker_(marker_, point_, point_color_, waypoint_goal, marker_mode_, waypoint_number);
 }
 
 void waypoint_rviz::way_point_corner_set_(vector<vector<string>>& waypoint_corner, uint16_t &waypoint_number)
@@ -159,8 +171,8 @@ void waypoint_rviz::way_point_corner_set_(vector<vector<string>>& waypoint_corne
   uint16_t array_number = waypoint_number - 1;
   waypoint_corner[array_number].push_back("corner");
   
-  marker_flag_ = 1;
-  waypoint_marker_(marker_, waypoint_corner, marker_flag_, waypoint_number);
+  marker_mode_ = 1;
+  waypoint_marker_(marker_, point_, point_color_, waypoint_corner, marker_mode_, waypoint_number);
 }
 
 void waypoint_rviz::finish_and_file_write_waypoint_(vector<vector<string>>& waypoint_file_write, uint16_t &waypoint_number)
@@ -187,50 +199,67 @@ void waypoint_rviz::finish_and_file_write_waypoint_(vector<vector<string>>& wayp
     ros::shutdown();
 }
 
-void waypoint_rviz::waypoint_marker_(visualization_msgs::Marker& marker, vector<vector<string>>& waypoint_goal_corner, bool &marker_flag, uint16_t &waypoint_number)
+void waypoint_rviz::waypoint_marker_(visualization_msgs::Marker& marker, geometry_msgs::Point& point, std_msgs::ColorRGBA& point_color, vector<vector<string>>& waypoint_goal_corner, bool &marker_mode, uint16_t &waypoint_number)
 {
   uint16_t array_number = waypoint_number - 1;
 
   marker.header.frame_id = "/map";
   marker.header.stamp = ros::Time::now();
 
-  marker.ns = "basic_shapes";
   marker.id = 0;
-
   marker.type = shape_;
 
   marker.action = visualization_msgs::Marker::ADD;
 
-  marker.pose.position.x = stof(waypoint_goal_corner[array_number][0]);
-  marker.pose.position.y = stof(waypoint_goal_corner[array_number][1]);
-  marker.pose.position.z = 0.2;
+  point.x = stof(waypoint_goal_corner[array_number][0]);
+  point.y = stof(waypoint_goal_corner[array_number][1]);
+  point.z = 0.2;
+  
+  marker.points.push_back(point);
 
-  marker.pose.orientation.z = stof(waypoint_goal_corner[array_number][2]);
-  marker.pose.orientation.w = stof(waypoint_goal_corner[array_number][3]);
+  marker.scale.x = 0.05;
+  marker.scale.y = 0.05;
 
-  marker.scale.x = 0.3;
-  marker.scale.y = 0.3;
-  marker.scale.z = 0.3;
-
-  if(!marker_flag)
+  if(!marker_mode)
   {
-    marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0;
+    point_color.r = 0.0f;
+    point_color.g = 1.0f;
+    point_color.b = 0.0f;
+    point_color.a = 1.0;
   }
 
-  else if(marker_flag)
+  else if(marker_mode)
   {
-    marker.color.r = 1.0f;
-    marker.color.g = 0.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0;
+    point_color.r = 1.0f;
+    point_color.g = 0.0f;
+    point_color.b = 0.0f;
+    point_color.a = 1.0;
   }
 
-  marker.lifetime = ros::Duration();
+  marker.colors.push_back(point_color);
+  
+  waypoint_marker_id_.push_back(array_number);
 
   marker_pub.publish(marker);
+
+  marker_flag_ = 1;
+}
+
+bool waypoint_rviz::waypoint_marker_id_check(vector<uint16_t>& waypoint_marker_id, uint16_t &waypoint_number)
+{
+  uint8_t flag = 0;
+  if(marker_flag_)
+  {
+    auto result = find(waypoint_marker_id.begin(), waypoint_marker_id.end(), waypoint_number);
+    
+    if(*result == waypoint_number)
+    {
+      waypoint_marker_id.pop_back();
+      flag = 1;
+    }
+  }
+
+  return flag;
 }
 
 int main(int argc, char** argv)
